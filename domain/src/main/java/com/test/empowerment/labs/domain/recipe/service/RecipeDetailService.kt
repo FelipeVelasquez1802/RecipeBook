7a64d77251aa3a8e81aa1e9ff9c563801d6a04c0
felipevelasquez08 <andres.diaz.velasquez@unillanos.edu.co>
2023-08-25 23:19:59 -0500

    🚀change: update steps in gradle.yml 🚀change: modify build.gradle of domain for convert android library to java library

--- domain/src/main/java/com/test/empowerment/labs/domain/recipe/service/RecipeDetailService.kt
+++ domain/src/main/java/com/test/empowerment/labs/domain/recipe/service/RecipeDetailService.kt
@@ -3,9 +3,8 @@ package com.test.empowerment.labs.domain.recipe.service
 import com.test.empowerment.labs.domain.exception.BadIdException
 import com.test.empowerment.labs.domain.recipe.model.RecipeDetail
 import com.test.empowerment.labs.domain.recipe.repository.RecipeDetailRepository
-import javax.inject.Inject
 
-class RecipeDetailService @Inject constructor(private val recipeDetailRepository: RecipeDetailRepository) :
+class RecipeDetailService(private val recipeDetailRepository: RecipeDetailRepository) :
     RecipeBaseService<RecipeDetailRepository>(recipeDetailRepository) {
     fun getRecipeDetail(id: Int): RecipeDetail {
         if (id < 0) {
--- infrastructure/src/main/java/com/test/empowerment/labs/infrastructure/recipe/dimodule/RecipeModule.kt
+++ infrastructure/src/main/java/com/test/empowerment/labs/infrastructure/recipe/dimodule/RecipeModule.kt
@@ -1,18 +1,11 @@
 package com.test.empowerment.labs.infrastructure.recipe.dimodule
 
-import android.content.Context
-import androidx.room.Room
 import com.test.empowerment.labs.domain.recipe.repository.RecipeRepository
-import com.test.empowerment.labs.infrastructure.common.database.DatabaseConfig
 import com.test.empowerment.labs.infrastructure.recipe.repository.RecipeRepositoryImpl
 import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
-import dagger.hilt.android.qualifiers.ApplicationContext
-import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
 
 
 @Module
